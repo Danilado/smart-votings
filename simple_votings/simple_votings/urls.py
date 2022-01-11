611ba56660252840485c9184e3b01c760ec91883
Горбунов Константин <quswadress@gmail.com>
2022-01-11 15:47:32 +0000

    Change user profile

--- simple_votings/simple_votings/urls.py
+++ simple_votings/simple_votings/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls.static import static
-from simple_votings import settings
+from django.urls import path, include
 
 from user_profile.views import *
 
--- simple_votings/user_profile/urls.py
+++ simple_votings/user_profile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from user_profile.views import register_user
+from user_profile.views import register_user, change_user
 
 urlpatterns = [
     path("", include('django.contrib.auth.urls')),
-    path("register", register_user)
+    path("register", register_user),
+    path("change", change_user)
 ]
