5bfa299219624b377854d319091dc270b81ee5f1
Катаев Илья <k4t4eff@gmail.com>
2022-01-14 21:06:40 +0000

    add comments

--- simple_votings/user_profile/forms.py
+++ simple_votings/user_profile/forms.py
@@ -11,19 +11,19 @@ from django.http import QueryDict
 class DescForm(forms.Form):
     CHOICES = [
         ('1', 'YES'), ('2', 'NO')
-    ]  # тот кто теперь должен делать подсчет, в бд хранится номер варианта ответа yes - 1, no - 2
+    ]  # в бд хранится номер варианта ответа: 1 - "yes", 2 - "no"
     choice_field = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
 
 
 
-class AddVoteForm(forms.Form):
+class AddVoteForm(forms.Form): # add new vote
     theme = forms.CharField(label="Название")
     description = forms.CharField(label="Описание")
     answers = forms.CharField(label="Введите варианты ответов, разделяя их знаком ; ")
 
 
 
-class RegistrationForm(forms.Form):
+class RegistrationForm(forms.Form): # register
     username = UsernameField(widget=TextInput(attrs={'autofocus': True}))
     password = forms.CharField(
         label=gettext("Password"),
