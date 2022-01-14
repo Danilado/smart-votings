e37f40e1edad9da5a44e04f46f4234790b5e4966
Катаев Илья <k4t4eff@gmail.com>
2022-01-14 20:43:21 +0000

    added comments

--- simple_votings/simple_votings/urls.py
+++ simple_votings/simple_votings/urls.py
@@ -5,13 +5,13 @@ from simple_votings.views import *
 from user_profile.views import *
 
 urlpatterns = [
-    path("", super_voleyball),
+    path("", super_voleyball), # super_volleyball
     path("admin/", admin.site.urls),
-    path("auth/", include('user_profile.urls')),
-    path("accounts/profile", get_user_profile_page),
+    path("auth/", include('user_profile.urls')), # login page
+    path("accounts/profile", get_user_profile_page), # profile page
     path("description/", description_vote),
-    path("show/", show_all),
-    path("add/", add_new_vote)
+    path("show/", show_all), # all votings
+    path("add/", add_new_vote) # new vote page
 ]
 
 urlpatterns += staticfiles_urlpatterns()
