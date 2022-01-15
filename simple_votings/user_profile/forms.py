4a43743302267f57f7698f386f6b8b4f0138ba82
Горбунов Константин <quswadress@gmail.com>
2022-01-15 06:10:50 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- simple_votings/user_profile/forms.py
+++ simple_votings/user_profile/forms.py
@@ -11,7 +11,7 @@ from django.http import QueryDict
 class DescForm(forms.Form):
     CHOICES = [
         ('1', 'YES'), ('2', 'NO')
-    ]  # quswadress:    тот кто теперь должен делать подсчет, в бд хранится номер варианта ответа yes - 1, no - 2
+    ]  # Vlastelin a.k.a kotuk: тот кто теперь должен делать подсчет, в бд хранится номер варианта ответа yes - 1, no - 2
        # gospodin:      но почему не 0 и 1 ?
     choice_field = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
 
