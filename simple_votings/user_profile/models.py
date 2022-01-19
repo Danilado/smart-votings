0258553dfeebb41b0162e39ce65eec7f3fff91b2
quswadress <quswadress@gmail.com>
2022-01-19 17:28:45 +0300

    Added report functionality

--- simple_votings/user_profile/models.py
+++ simple_votings/user_profile/models.py
@@ -1,16 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from base import BaseModel
 
 
-class UserVote(BaseModel): # голос пользователя
+class UserVote(BaseModel):  # голос пользователя
     description = models.TextField()
     answer1 = models.TextField()
     answer2 = models.TextField()
     result = models.TextField()
 
 
-class Vote(BaseModel): # голосование
+class Vote(BaseModel):  # голосование
     theme = models.TextField()
     description = models.TextField()
     answers = models.TextField()
+
+
+class Report(BaseModel):
+    author = models.ForeignKey(get_user_model(), models.CASCADE)
+    theme = models.TextField()
+    content = models.TextField()
